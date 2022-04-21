9e1de4ed639a1c32e8ed71201c59a8567f218198
jmacd87 <jmacd87@gmail.com>
2022-04-21 15:32:13 -0300

    fix API Key

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
 const baseUrl = 'https://api.openweathermap.org/data/2.5';
-const apiKey = process.env.REACT_APP_API_KEY
 
 export const fetchWeatherData = async (city: string) => {
-    let url = `${baseUrl}/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    let url = `${baseUrl}/forecast?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
     return await (await fetch(url)).json();
 };
